58cf390b81dc900726675de054b87a982caddc29
kaushik-manoharan <kaushik.manoharan@tracxn.com>
2020-09-28 18:06:22 +0530

    after async await

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,60 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import { companiesAPI } from '../../containers/companiesApi'
-import Navigation from '../utils/navbar'
-export default function HomePage(){
-    const [res,setRes] = useState({data:[],count:0});
-    const [frm,setFrm] = useState(0);
-    const [load,setLoad] = useState(false)
-    useEffect(()=>{
-        console.log(window.innerHeight, document.documentElement.scrollTop,window.innerHeight+document.documentElement.scrollTop, document.documentElement.offsetHeight)
+/** @format */
 
-        window.addEventListener('scroll',handleScroll)
-companiesAPI(res,setRes,frm,setFrm);
+import React, { useEffect, useState } from "react";
+import { companiesAPI } from "../../containers/companiesApi";
+import Navigation from "../utils/navbar";
+export default function HomePage() {
+  const [res, setRes] = useState({ data: [], count: 0 });
+  const [frm, setFrm] = useState(0);
+  const [load, setLoad] = useState(false);
 
+  async function insideFn() {
+    await companiesAPI(res, setRes, frm, setFrm);
+  }
+  useEffect(() => {
+    console.log(
+      window.innerHeight,
+      document.documentElement.scrollTop,
+      window.innerHeight + document.documentElement.scrollTop,
+      document.documentElement.offsetHeight
+    );
+    window.addEventListener("scroll", handleScroll);
 
-    },[])
-    // 
-    const handleScroll = () => { 
-// console.log(window.innerHeight, document.documentElement.scrollTop,window.innerHeight+document.documentElement.scrollTop, document.documentElement.offsetHeight-20)
+    insideFn();
+  }, []);
+  //
+  const handleScroll = () => {
+    // console.log(window.innerHeight, document.documentElement.scrollTop,window.innerHeight+document.documentElement.scrollTop, document.documentElement.offsetHeight-20)
 
-if(window.innerHeight+document.documentElement.scrollTop>document.documentElement.offsetHeight-20){
-    setLoad(true)
-}
-      };      
-    // 
+    if (
+      window.innerHeight + document.documentElement.scrollTop >
+      document.documentElement.offsetHeight - 20
+    ) {
+      setLoad(true);
+    }
+  };
+  //
 
-    useEffect(()=>{
-        if(document.documentElement.offsetHeight<window.innerHeight&&!load)
-        companiesAPI(res,setRes,frm,setFrm)
-        if(load){
-            companiesAPI(res,setRes,frm,setFrm)
-            setLoad(false)
-        }
-    },[load,res])
+  useEffect(() => {
+    if (document.documentElement.offsetHeight < window.innerHeight && !load)
+      companiesAPI(res, setRes, frm, setFrm);
+    if (load) {
+      insideFn();
+      setLoad(false);
+    }
+  }, [load, res]);
 
-    return(
-        <>
-        <Navigation/>
-        <table>
-            <tr style={{border: '1px solid black'}}>
-                <th style={{border: '1px solid black'}}>Serial no</th>
-                <th style={{border: '1px solid black'}}>Id</th>
-                <th style={{border: '1px solid black'}}>Name</th>
-                <th style={{border: '1px solid black'}}>Domain</th>
-                <th style={{border: '1px solid black'}}>Location (primary Country)</th>
-                <th style={{border: '1px solid black'}}>Founded year</th>
-            </tr>
-        {res&&res.data.map((f,i)=>{return  <tr key = {i} style={{border: '1px solid black'}}>
-        <td style={{border: '1px solid black'}}>{i+1}</td>
-            <td style={{border: '1px solid black'}}>{f.id}</td>
-        <td style={{border: '1px solid black'}}>{f.name}</td>
-        <td style={{border: '1px solid black'}}>{f.domain}</td>
-        <td style={{border: '1px solid black'}}>{f.location&&f.location.country}</td>
-        <td style={{border: '1px solid black'}}>{f.foundedYear}</td>
-            </tr>
-
-        })}
-        </table>
-        </>
-    )
+  return (
+    <>
+      <Navigation />
+      <table>
+        <tr style={{ border: "1px solid black" }}>
+          <th style={{ border: "1px solid black" }}>Serial no</th>
+          <th style={{ border: "1px solid black" }}>Id</th>
+          <th style={{ border: "1px solid black" }}>Name</th>
+          <th style={{ border: "1px solid black" }}>Domain</th>
+          <th style={{ border: "1px solid black" }}>
+            Location (primary Country)
+          </th>
+          <th style={{ border: "1px solid black" }}>Founded year</th>
+        </tr>
+        {res &&
+          res.data.map((f, i) => {
+            return (
+              <tr key={i} style={{ border: "1px solid black" }}>
+                <td style={{ border: "1px solid black" }}>{i + 1}</td>
+                <td style={{ border: "1px solid black" }}>{f.id}</td>
+                <td style={{ border: "1px solid black" }}>{f.name}</td>
+                <td style={{ border: "1px solid black" }}>{f.domain}</td>
+                <td style={{ border: "1px solid black" }}>
+                  {f.location && f.location.country}
+                </td>
+                <td style={{ border: "1px solid black" }}>{f.foundedYear}</td>
+              </tr>
+            );
+          })}
+      </table>
+    </>
+  );
 }
--- src/containers/companiesApi.js
+++ src/containers/companiesApi.js
@@ -1,32 +1,42 @@
-function companiesAPI(res,setRes,frm,setFrm){
-    let reqOptions = {
-        method: 'POST',
-        // mode: 'no-cors',
-        headers: {"accessToken":  '37b1358d-1864-4d8f-8390-848ce2f4faaa', 
-        "X-Request-Source":"KaushikManoharan_API_call",
-        "sessionToken":"99ebc12a-5bde-4156-9661-83dc581da212",
-            "Content-Type": 'application/json'
-            },
-            
-        body: JSON.stringify({
-            from: frm,
-        })
-    }
-fetch("https://tracxn.com/api/2.2/companies",reqOptions)
-    .then(res => {
-        if(res.ok) return res.json()
-    throw new Error(res)})
-    .then(data=>{
-        if(res.data.length<=data.total_count){
-        setFrm(frm+20)
-        if(res.data.length){
-            setRes({data: res.data.concat(data.result),count: data.total_count})
-        }
-        else{
-            setRes({data: data.result, count: data.total_count})
-        }
-        }
+/** @format */
+
+let loading = false;
+function companiesAPI(res, setRes, frm, setFrm) {
+  let reqOptions = {
+    method: "POST",
+    headers: {
+      accessToken: "37b1358d-1864-4d8f-8390-848ce2f4faaa",
+      "X-Request-Source": "KaushikManoharan_API_call",
+      sessionToken: "99ebc12a-5bde-4156-9661-83dc581da212",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      from: frm,
+    }),
+  };
+  loading = true;
+  fetch("https://tracxn.com/api/2.2/companies", reqOptions)
+    .then((res) => {
+      if (res.ok) return res.json();
+      throw new Error(res);
     })
-    .catch(e=>console.log(e))
+    .then((data) => {
+      if (res.data.length <= data.total_count) {
+        setFrm(frm + 20);
+        if (res.data.length) {
+          setRes({
+            data: res.data.concat(data.result),
+            count: data.total_count,
+          });
+        } else {
+          setRes({ data: data.result, count: data.total_count });
+        }
+      }
+      loading = false;
+    })
+    .catch((e) => {
+      loading = false;
+      console.log(e);
+    });
 }
-export {companiesAPI}
+export { companiesAPI };
